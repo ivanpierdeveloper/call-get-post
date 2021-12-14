9b831d0bf156aaaa03a248fd7dd273ede916a0ba
ivanpierdeveloper <95723620+ivanpierdeveloper@users.noreply.github.com>
2021-12-14 16:53:09 +0100

    ivanpier

--- docs/script.js
+++ docs/script.js
@@ -38,7 +38,7 @@ const myFunc = {
   controlloAndSend : function() {
    var inputElement =  document.querySelectorAll('.input-text');
    inputElement.forEach(function(val, indice) {
-    console.log(val.value);
+    console.log(` Valore : ${val.value} Indice : ${indice}`);
    });
     
   }
