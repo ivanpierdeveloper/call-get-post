077c928ef17f9a000be14d97d720ed1a58edb765
ivanpierdeveloper <95723620+ivanpierdeveloper@users.noreply.github.com>
2021-12-14 14:01:26 +0100

    ivanpier

--- docs/script.js
+++ docs/script.js
@@ -2,5 +2,9 @@
 const myFunc = {
   messageToBack : function() {
     alert("Welcome To Back!!!");
+  },
+  messaggioMyAlert : function(testo) {
+    let spanalert = document.querySelector('.my-alert-testo');
+    spanalert.innerHTML = testo;
   }
 }
