65c65aded3623f6eac7f804a632e55200ae295df
ivanpierdeveloper <95723620+ivanpierdeveloper@users.noreply.github.com>
2021-12-14 14:32:16 +0100

    ivanpier

--- docs/script.js
+++ docs/script.js
@@ -6,5 +6,26 @@ const myFunc = {
   messaggioMyAlert : function(testo) {
     let spanalert = document.querySelector('.my-alert-testo');
     spanalert.innerHTML = testo;
+  },
+  showAlert : function(data) {
+    $('.full-screen').css({
+      'display' : 'block'
+    });
+    $('.my-alert').css({
+        'position' : 'fixed',
+        'top' : '50%',
+        'transition' : '2s'
+    });
+    myFunc.messaggioMyAlert(data);
+  },
+  closeMyAlert : function() {
+    $('.full-screen').css({
+      'display' : 'none'
+    });
+    $('.my-alert').css({
+      'position' : 'fixed',
+      'top' : '-50%',
+      'transition' : '2s'
+    });
   }
-}
+} // end const myFunc
