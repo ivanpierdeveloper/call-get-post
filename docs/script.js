538609fe7733947c1f06f4ef25e0affc370c5dfe
ivanpierdeveloper <95723620+ivanpierdeveloper@users.noreply.github.com>
2021-12-15 11:28:09 +0100

    a

--- docs/script.js
+++ docs/script.js
@@ -95,9 +95,9 @@ async function senddata(url, email, usr, psw, age) {
     formData.set("age", age);
 
     let headers = new Headers();
-      headers.append('Content-Type', 'application/json');
+      /* headers.append('Content-Type', 'application/json');
       headers.append('Accept', 'application/json');
-      headers.append('Authorization', 'Basic ' + base64.encode(usr + ":" +  psw));
+      headers.append('Authorization', 'Basic ' + base64.encode(usr + ":" +  psw)); */
       headers.append('Origin',url);
 
     const request = new Request(url, {
