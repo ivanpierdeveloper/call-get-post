78afd2e1f8cc0a40f85639b97d370c6b27ca1adb
ivanpierdeveloper <95723620+ivanpierdeveloper@users.noreply.github.com>
2021-12-16 17:24:17 +0100

    ivanpier

--- docs/script.js
+++ docs/script.js
@@ -180,8 +180,10 @@ async function senddata(url, email, usr, psw, age) {
 class Clsmultyfunction {
   createElement(el) {
     const divContent = document.querySelector('.content');
+
     var numDiv = 0;
     numDiv += 1;
+    
     let title = document.createElement('h5'); // title
     title.style.setProperty('margin', '0 auto');
 
