e80d4ff9bd01b46fc33c840b2c8559c4c387f7cd
ivanpierdeveloper <95723620+ivanpierdeveloper@users.noreply.github.com>
2021-12-16 16:57:06 +0100

    ivanpier

--- docs/script.js
+++ docs/script.js
@@ -137,13 +137,21 @@ async function senddata(url, email, usr, psw, age) {
       
       var ul = document.createElement('ul');
       data.forEach(function(val, indice) {
-        var li = document.createElement('li');
-        li.appendChild(document.createTextNode(`id: ${val.id}`));
-        li.appendChild(document.createTextNode(`e-mail: ${val.email}`));
-        li.appendChild(document.createTextNode(`usr: ${val.usr}`));
-        li.appendChild(document.createTextNode(`psw: ${val.psw}`));
-        li.appendChild(document.createTextNode(`age: ${val.age}`));
-        ul.appendChild(li);
+        var liId    = document.createElement('li');
+        var liEmail = document.createElement('li');
+        var liUsr   = document.createElement('li');
+        var liPsw   = document.createElement('li');
+        var liAge   = document.createElement('li');
+        liId.appendChild(document.createTextNode(`id: ${val.id}`));
+        liEmail.appendChild(document.createTextNode(`e-mail: ${val.email}`));
+        liUsr.appendChild(document.createTextNode(`usr: ${val.usr}`));
+        liPsw.appendChild(document.createTextNode(`psw: ${val.psw}`));
+        liAge.appendChild(document.createTextNode(`age: ${val.age}`));
+        ul.appendChild(liId);
+        ul.appendChild(liEmail);
+        ul.appendChild(liUsr);
+        ul.appendChild(liPsw);
+        ul.appendChild(liAge);
       }); // ./forEach
       const cls = new Clsmultyfunction();
       cls.createElement(ul);
