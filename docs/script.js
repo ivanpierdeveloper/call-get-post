7c52d6f06dd17c072d6880869b43580160b47be0
ivanpierdeveloper <95723620+ivanpierdeveloper@users.noreply.github.com>
2021-12-16 09:59:48 +0100

    ivanpier

--- docs/script.js
+++ docs/script.js
@@ -112,13 +112,13 @@ async function senddata(url, email, usr, psw, age) {
         // headers.append("Access-Control-Expose-Headers", "Content-Length, X-JSON");
         // headers.append('Access-Control-Max-Age',  '86400');
         // headers.append('Content-Type', 'application/json; charset=UTF-8');
-        headers.append("Content-Type", "application/x-www-form-urlencoded"); // OK POST
-        headers.append("Access-Control-Allow-Origin", "*");
+        // headers.append("Content-Type", "application/x-www-form-urlencoded");
+        // headers.append("Access-Control-Allow-Origin", "*");
         //headers.append('Content-Type', 'text/json', 'Access-Control-Allow-Origin'); // Controllo di accesso Consenti all'origine
     const request = new Request(url, {
       method: 'POST',
       //mode: 'cors',
-      headers: headers,
+      // headers: headers,
       body: formData
     });
     await fetch(request)
